a5f77adbebfe8c260ac287147f5eb87187ba8507
lochleaf <200536258@student.georgianc.on.ca>
2026-03-17 11:47:35 -0400

    made constructor
    I made constructor for the abstract class and assigning the private strings

--- student.java
+++ student.java
@@ -1,7 +1,13 @@
 public abstract class student {
     
-private String name;
-private String ID;
-private String email;
+    private String name;
+    private String ID;
+    private String email;
+
+    public student(String name, String ID, String email){
+        this.name = name;
+        this.ID = ID;
+        this.email = email;
+    }
 
 }
