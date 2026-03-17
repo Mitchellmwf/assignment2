29fc1bb1df3213bc3cd5c583226bec287ccfaf84
lochleaf <200536258@student.georgianc.on.ca>
2026-03-17 15:13:37 -0400

    added constructor
    added constructor for instructor class that extends to abstract person class that graps name id and email and after ill add the override

---
+++ Instructor.java
@@ -0,0 +1,11 @@
+public class Instructor extends Person{
+    private String department;
+
+    public Instructor(String name, String id, String email, String department){
+
+        super(name, id, email);
+        this.department = department;
+
+    }
+
+}
