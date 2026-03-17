7b10017b203de10c5f36b518438a0a7164ebd598
Caleb <caleb.hunt789@gmail.com>
2026-03-17 20:37:34 +0000

    moving my part to another file

---
+++ Studentmanager.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+// Main program
+public class StudentManager {
+    
+    private static ArrayList<Student> students = new ArrayList<>();
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
