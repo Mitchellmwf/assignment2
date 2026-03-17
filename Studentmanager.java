54848b67504d34182ca0249a82dd1d15d696dcdd
Caleb <caleb.hunt789@gmail.com>
2026-03-17 20:39:10 +0000

    i think the scanner part is good

--- Studentmanager.java
+++ Studentmanager.java
@@ -9,3 +9,36 @@ public class StudentManager {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+
+        addExampleStudents();
+
+        boolean running = true;
+        
+        while (running) {
+            showMenu();
+            int choice = getChoice();
+
+            if (choice == 1) {
+                addStudent();
+            }
+            else if (choice == 2) {
+                removeStudentById();
+            }
+            else if (choice == 3) {
+                displayAllStudents();
+            }
+            else if (choice == 4) {
+                searchStudents();
+            }
+            else if (choice == 5) {
+                running = false;
+                System.out.println("\nThank you! Goodbye.");
+            }
+            else {
+                System.out.println("Invalid choice. Please enter 1-5.");
+            }
+            
+            System.out.println();
+        }
+        
+        scanner.close();
