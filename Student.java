20cdcd26562a52f5b4a908d98cab28abe372f633
lochleaf <200536258@student.georgianc.on.ca>
2026-03-17 14:59:11 -0400

    added constructor
    added constructor to student class that takes from person class

--- Student.java
+++ Student.java
@@ -1,3 +1,10 @@
 public class Student extends Person {
-    
+
+    private String program;
+
+    public Student(String name, String id, String email, String program) {
+        super(name, id, email);
+        this.program = program;
+    }
+
 }
