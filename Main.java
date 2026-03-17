a298ce714e36f3abfdcd8b1be6e4709924a60f47
lochleaf <200536258@student.georgianc.on.ca>
2026-03-17 15:40:04 -0400

    added polymorphism
    added polymorphism to main for instructor and student

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
 
+    public static void main(String[] args) {
+        Person s = new Student("Peter", "123456", "puterpeanut@email.com", "Culinary Arts");
+        Person i = new Instructor("Professor. Victor", "658432", "victornation@email.com", "Science");
     
+    }
     
 }
