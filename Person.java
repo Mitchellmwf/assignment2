bfaf7fed3745c72bd9c47e87b4563551f48ea068
lochleaf <200536258@student.georgianc.on.ca>
2026-03-17 14:51:14 -0400

    added getters and setters
    Made getters with validation to check if empty or spacing also numbers up to 6 using regex and email check

--- Person.java
+++ Person.java
@@ -1,13 +1,52 @@
 public abstract class Person {
     
     private String name;
-    private String ID;
+    private String id;
     private String email;
 
-    public Person(String name, String ID, String email){
+    public Person(String name, String id, String email){
         this.name = name;
-        this.ID = ID;
+        this.id = id;
         this.email = email;
+    }
+
+    public void setName(String name) {
+        if(name != null && !name.trim().isEmpty()){
+            this.name = name;
+        }
+        else {
+            System.out.println("Invalid name.");
+        }
+    }
+
+    public void setID(String id) {
+        if(id != null && id.matches("\\d{1,6}")){
+            this.id = id;
+        }
+        else {
+            System.out.println("Invalid ID.");
+        }
+    }
+
+    public void setEmail(String email) {
+        if(email != null && email.contains("@") && !email.trim().isEmpty()){
+            this.email = email;
+        }
+        else {
+            System.out.println("Invalid email.");
+        }
+    }
+
+    public String getName() {
+    return name;
+}
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public abstract String getDetails();
